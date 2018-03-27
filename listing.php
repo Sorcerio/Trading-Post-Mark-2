a6915c3f2e58dbeff0f9aa8791048e011591aec0
maximombro <maximombro@gmail.com>
2018-03-27 19:37:05 -0400

    Added files for all pages

---
+++ listing.php
@@ -0,0 +1,8 @@
+<!-- Header -->
+<?php include ("assets/header.php"); ?>
+
+<!-- Content Start -->
+<h1>Trading Hub Mark 2 : Listing</h1>
+
+<!-- Footer -->
+<?php include ("assets/footer.php"); ?>
---
+++ login.php
@@ -0,0 +1,8 @@
+<!-- Header -->
+<?php include ("assets/header.php"); ?>
+
+<!-- Content Start -->
+<h1>Trading Hub Mark 2 : Log-In</h1>
+
+<!-- Footer -->
+<?php include ("assets/footer.php"); ?>

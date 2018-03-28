83b3243f35a3441169a455d3b8f57c9153f72d69
maximombro <maximombro@gmail.com>
2018-03-27 23:19:43 -0400

    Tweaked index CSS and HTML

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 <div class="flexContainer" id="top5Container">
 	<div>
 		<div class="leftPanel">
-			<img src="images/noImage.png" alt="TITLE_OF_LISTING Image 1">
+			<img src="LISTING_ADDRESS" alt="LISTING_TITLE Image 1" onerror="this.src = 'images/noImage.png';">
 		</div>
 		<div class="rightPanel">
-			<h2>TITLE_OF_LISTING</h2>
+			<h2>LISTING_TITLE</h2>
 			<p>Phasellus non orci non sapien imperdiet accumsan ac nec libero. Aliquam elementum metus non hendrerit aliquet. Maecenas imperdiet ligula in gravida suscipit.</p>
 		</div>
 	</div>

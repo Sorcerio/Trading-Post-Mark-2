6835a5acecbf32f17c8176a53e21a5d3060f703e
maximombro <maximombro@gmail.com>
2018-03-27 20:25:21 -0400

    Added pages to the nav

--- assets/header.php
+++ assets/header.php
@@ -1,3 +1,8 @@
+<?php
+	$phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
+	$path_parts = pathinfo($phpSelf);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,13 +17,16 @@
 	<link rel="stylesheet" type="text/css" href="css/mainStyle.css">
 </head>
 
-<body>
+<body <?php print 'id="'.$path_parts['filename'].'"'?>>
 	<!-- Navigation -->
 	<nav class="topNav" id="mainTopNav">
-		<a href="#home" >Home</a>
-		<a href="#news">News</a>
-		<a href="#contact" class="active">Contact</a>
-		<a href="#about">About</a>
+		<a href="index.php" <?php if($path_parts['filename'] == "index"){print 'class="active"';} ?>>Home</a>
+		<a href="submit.php" <?php if($path_parts['filename'] == "submit"){print 'class="active"';} ?>>Submit</a>
+		<a href="browse.php" <?php if($path_parts['filename'] == "browse"){print 'class="active"';} ?>>Browse</a>
+		<a href="account.php" <?php if($path_parts['filename'] == "account"){print 'class="active"';} ?>>Account</a>
+
+		<?php print $path_parts; ?>
+
 		<a href="javascript:void(0);" class="icon" onclick="setResponsiveNav()">&#9776;</a>
 	</nav>
 

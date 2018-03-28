ca3bfdaaa4b3211e7a16b812e7d8f8f3347de935
maximombro <maximombro@gmail.com>
2018-03-27 22:53:48 -0400

    Completed index CSS

--- index.php
+++ index.php
@@ -2,7 +2,20 @@
 <?php include ("assets/header.php"); ?>
 
 <!-- Content Start -->
-<h1>Trading Hub Mark 2 : Index</h1>
+<h1>Welcome to the Trading Hub</h1>
+
+<!-- Top 5 Items -->
+<div class="flexContainer" id="top5Container">
+	<div>
+		<div class="leftPanel">
+			<img src="images/noImage.png" alt="TITLE_OF_LISTING Image 1">
+		</div>
+		<div class="rightPanel">
+			<h2>TITLE_OF_LISTING</h2>
+			<p>Phasellus non orci non sapien imperdiet accumsan ac nec libero. Aliquam elementum metus non hendrerit aliquet. Maecenas imperdiet ligula in gravida suscipit.</p>
+		</div>
+	</div>
+</div>
 
 <!-- Footer -->
 <?php include ("assets/footer.php"); ?>

e70089a622e20ed39f659a7dc5be5257b8b79ceb
maximombro <maximombro@gmail.com>
2018-04-09 14:16:03 -0400

    Styled Account page and began Acccound page backend

--- account.php
+++ account.php
@@ -2,13 +2,7 @@
 <?php include ("assets/header.php"); ?>
 
 <?php
-    // TESTING INFO. REMOVE WHEN DONE WITH TESTING.
-    if(isset($_SESSION['login'])) {
-        print 'AccountID: '.$_SESSION['login'];
-    } else {
-        print 'AccountID: NO DATA';
-    }
-
+    // Logout Tag
     if(isset($_GET['logout'])) {
         session_unset();
         session_destroy();
@@ -16,15 +10,78 @@
 ?>
 
 <?php
-    // Include the account controls
-    include "actions/accountControls.php";
+    // Check for logged in redirect
+    if(!isset($_SESSION['login'])) {
+        // Relocate to account page because user is already logged in
+        ob_start();
+        header("Location: login.php");
+        ob_end_flush();
+        die();
+    } else {
+        // Include the account controls
+        include "actions/accountControls.php";
 
-    // Pull account data
-    $data = $node->getAccountInfoFromIdPHP(4);
+        // Pull account data
+        $data = $node->getAccountInfoFromIdPHP($_SESSION['login']);
+    }
 ?>
 
 <!-- Content Start -->
 <h1 class="jumboHeader">Hello, <?php print $data['name']; ?></h1>
+
+<div class="flexContainer accountBoxes">
+    <div>
+        <h2>User Info</h2>
+        <p>Your Email:<br>
+            <span><?php print $data['email']; ?></span>
+        </p>
+        <p>Your IP:<br>
+            <span><?php print $_SERVER['REMOTE_ADDR']; ?></span>
+        </p>
+        <p>Active Products:<br>
+            <span>-1</span>
+        </p>
+    </div>
+    <div>
+        <h2>Actions</h2>
+        <button onclick="showProducts()">Show Products</button>
+        <button onclick="showChat()">Show Chat</button>
+    </div>
+    <div>
+        <h2>Controls</h2>
+        <button onclick="showChangePass()">Change Password</button>
+        <button onclick="logoutUser()">Log Out</button>
+        <button onclick="deleteAccount()">Delete Account</button>
+    </div>
+</div>
+
+<!-- Button Controls -->
+<script>
+    // Shows/Hides the Products list
+    function showProducts() {
+        alert("Products Triggered");
+    }
+
+    // Shows/Hides the Chat list
+    function showChat() {
+        alert("Chat Triggered");
+    }
+
+    // Shows/Hides the change password menu
+    function showChangePass() {
+        alert("Pass Triggered");
+    }
+
+    // Logs out the user
+    function logoutUser() {
+        alert("User Triggered");
+    }
+
+    // Shows/Hides the delete account menu
+    function deleteAccount() {
+        alert("Delete Triggered");
+    }
+</script>
 
 <!-- Footer -->
 <?php include ("assets/footer.php"); ?>

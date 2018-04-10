adabe0d02a50499b16825581e40465b91054ff66
maximombro <maximombro@gmail.com>
2018-04-09 21:13:20 -0400

    Added JavaScript to account page for panels

--- account.php
+++ account.php
@@ -44,32 +44,89 @@
     </div>
     <div>
         <h2>Actions</h2>
-        <button onclick="showProducts()">Show Products</button>
-        <button onclick="showChat()">Show Chat</button>
+        <button onclick="showProducts()" id="productsButton">Show Products</button>
+        <button onclick="showChat()" id="chatButton">Show Chat</button>
+    </div>
+    <div id="productsPanel">
+        <h2>Your Products</h2>
+        <ol>
+            <li>
+                <a href="listing.php?id=PRODUCT_ID">
+                    <h3>PRODUCT_NAME</h3>
+                    <p>PRODUCT_PRICE | PRODUCT_DATE</p>
+                </a>
+            </li>
+        </ol>
+    </div>
+    <div id="chatPanel">
+        <h2>Your Chat</h2>
     </div>
     <div>
         <h2>Controls</h2>
-        <button onclick="showChangePass()">Change Password</button>
-        <button onclick="logoutUser()">Log Out</button>
-        <button onclick="deleteAccount()">Delete Account</button>
+        <button onclick="showChangePass()" id="changePassButton">Show Change Password</button>
+        <button onclick="logoutUser()" id="logoutButton">Log Out</button>
+        <button onclick="showDeleteAccount()" id="deltAccButton">Show Delete Account</button>
+    </div>
+    <div id="changePasswordPanel">
+        <h2>Change Password</h2>
+        <form action="">
+        </form>
+    </div>
+    <div id="deleteAccountPanel">
+        <h2>Delete Account</h2>
+        <form action="">
+        </form>
     </div>
 </div>
 
 <!-- Button Controls -->
 <script>
+    // Variables
+    var products = false;
+    var chat = false;
+    var changePass = false;
+    var deleteAcc = false;
+
+    // Startup Protocol
+    startUpProtocol();
+
     // Shows/Hides the Products list
     function showProducts() {
-        alert("Products Triggered");
+        if(products) {
+            products = false;
+            document.getElementById("productsPanel").style.display = "none";
+            document.getElementById("productsButton").innerHTML = "Show Products";
+        } else {
+            products = true;
+            document.getElementById("productsPanel").style.display = "block";
+            document.getElementById("productsButton").innerHTML = "Hide Products";
+        }
     }
 
     // Shows/Hides the Chat list
     function showChat() {
-        alert("Chat Triggered");
+        if(chat) {
+            chat = false;
+            document.getElementById("chatPanel").style.display = "none";
+            document.getElementById("chatButton").innerHTML = "Show Chat";
+        } else {
+            chat = true;
+            document.getElementById("chatPanel").style.display = "block";
+            document.getElementById("chatButton").innerHTML = "Hide Chat";
+        }
     }
 
     // Shows/Hides the change password menu
     function showChangePass() {
-        alert("Pass Triggered");
+        if(changePass) {
+            changePass = false;
+            document.getElementById("changePasswordPanel").style.display = "none";
+            document.getElementById("changePassButton").innerHTML = "Show Change Password";
+        } else {
+            changePass = true;
+            document.getElementById("changePasswordPanel").style.display = "block";
+            document.getElementById("changePassButton").innerHTML = "Hide Change Password";
+        }
     }
 
     // Logs out the user
@@ -78,8 +135,35 @@
     }
 
     // Shows/Hides the delete account menu
-    function deleteAccount() {
-        alert("Delete Triggered");
+    function showDeleteAccount() {
+        if(deleteAcc) {
+            deleteAcc = false;
+            document.getElementById("deleteAccountPanel").style.display = "none";
+            document.getElementById("deltAccButton").innerHTML = "Show Delete Account";
+        } else {
+            deleteAcc = true;
+            document.getElementById("deleteAccountPanel").style.display = "block";
+            document.getElementById("deltAccButton").innerHTML = "Hide Delete Account";
+        }
+    }
+
+    // Runs when the page is started
+    function startUpProtocol() {
+        if(!products) {
+            document.getElementById("productsPanel").style.display = "none";
+        }
+
+        if(!chat) {
+            document.getElementById("chatPanel").style.display = "none";
+        }
+
+        if(!changePass) {
+            document.getElementById("changePasswordPanel").style.display = "none";
+        }
+
+        if(!deleteAcc) {
+            document.getElementById("deleteAccountPanel").style.display = "none";
+        }
     }
 </script>
 
